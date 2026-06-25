83867d4ea0c970b6884fca90b623a0b60dcf7243
dav3park <davepark1007@gmail.com>
2026-06-25 19:41:18 +0900

    chore: 초기 프로젝트 설정 (Spring boots 3.5.15 + Security + JWT + H2 + Swagger)

---
+++ src/main/java/com/dcom/intranet/dto/auth.java
@@ -0,0 +1,4 @@
+package com.dcom.intranet.dto;
+
+public class auth {
+}
---
+++ src/test/java/com/dcom/intranet/DcomIntranetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcom.intranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcomIntranetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
