e3c4001d8dc859feef0563181466d7121cff2ff5
dav3park <davepark1007@gmail.com>
2026-06-30 17:16:47 +0900

    feat: 이메일 인증코드 send, 인증코드 verify 구현

---
+++ src/main/java/com/dcom/intranet/domain/EmailVerification.java
@@ -0,0 +1,57 @@
+package com.dcom.intranet.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "email_verifications")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private boolean vertified;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public EmailVerification(String email, String code, int expirationMinutes){
+        this.email = email;
+        this.code = code;
+        this.expiresAt = LocalDateTime.now().plusMinutes(expirationMinutes);
+        this.vertified = false;
+    }
+
+    /// 인증완료처리
+    public void verify(){
+        this.vertified = true;
+    }
+
+    /// 만료 여부 확인
+    public boolean isExpired(){
+        return LocalDateTime.now().isAfter(this.expiresAt);
+    }
+}
---
+++ src/main/java/com/dcom/intranet/dto/auth/EmailVerifyRequest.java
@@ -0,0 +1,19 @@
+package com.dcom.intranet.dto.auth;
+
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailVerifyRequest {
+    @NotBlank(message = "이메일을 입력하지 않았습니다..")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "인증 코드를 입력하지 않았습니다.")
+    private String verificationCode;
+}
+
