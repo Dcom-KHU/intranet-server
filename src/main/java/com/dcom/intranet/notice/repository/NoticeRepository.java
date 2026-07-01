b1abd56f5c740ddadc667a29dc9cd6ad77a4eae7
SJLEE-03 <sjleego03@gmail.com>
2026-07-02 02:31:28 +0900

    fix: 공지사항 관련 기능 수정

--- src/main/java/com/dcom/intranet/notice/repository/NoticeRepository.java
+++ src/main/java/com/dcom/intranet/notice/repository/NoticeRepository.java
@@ -1,7 +1,11 @@
 package com.dcom.intranet.notice.repository;
 
 import com.dcom.intranet.notice.domain.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+    Page<Notice> findByTitleContaining(String title, Pageable pageable);
 }
