9ffbcbf16a8b267ab2d8b5ee04de5bb158266d14
하성준 <hasungjun@haseongjun-ui-MacBookPro.local>
2026-07-08 21:39:48 +0900

    Unify archive search keyword

--- src/main/java/com/dcom/intranet/archive/repository/ArchiveRepository.java
+++ src/main/java/com/dcom/intranet/archive/repository/ArchiveRepository.java
@@ -14,10 +14,12 @@ public interface ArchiveRepository extends JpaRepository<Archive, Long> {
     // 용도 : 족보 등록 시 기존 과목/교수 폴더가 있는지 확인용(보류)
     // "확률 및 랜덤변수", "확률및랜덤변수" 등의 여러 형태로 폴더를 생성할려는 경우가 생길 수 있으니 있는지 조회를 할 때는 띄어쓰기 없이 검색
     Optional<Archive> findBySubjectNameAndProfessorName(String subjectName, String professorName);
-    // 과목명으로 찾기
-    Page<Archive> findBySubjectNameContaining(String subjectName, Pageable pageable);
-    // 교수명으로 찾기
-    Page<Archive> findByProfessorNameContaining(String professorName, Pageable pageable);
+    // 과목명 또는 교수명으로 찾기
+    Page<Archive> findBySubjectNameContainingOrProfessorNameContaining(
+            String subjectName,
+            String professorName,
+            Pageable pageable
+    );
 
     //findAll(Pageable pageable)은 JpaRepository에 이미 있어서 작성 X
 }
