d94a91e91f603ddd8a2c342c81f6b115502406ad
SJLEE-03 <sjleego03@gmail.com>
2026-07-31 15:47:54 +0900

    검색엔진 수정

--- src/main/java/com/dcom/intranet/notice/repository/NoticeRepository.java
+++ src/main/java/com/dcom/intranet/notice/repository/NoticeRepository.java
@@ -4,12 +4,24 @@ import com.dcom.intranet.notice.domain.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     Page<Notice> findByTitleContaining(String title, Pageable pageable);
+
+    @Query("""
+            SELECT n
+            FROM Notice n
+            WHERE REPLACE(n.title, ' ', '') LIKE CONCAT('%', :keyword, '%')
+            """)
+    Page<Notice> searchByTitleIgnoringSpaces(
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
 
     List<Notice> findByAuthorId(Long authorId);
 }
